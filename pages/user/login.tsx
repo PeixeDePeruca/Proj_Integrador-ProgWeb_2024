01001eda2b3a7afdcbad89c314fcb38b4086bdb1
PeixeDePeruca <twitterluizin@gmail.com>
2024-09-05 17:38:27 -0300

    commit login 05/09/24

---
+++ pages/user/login.tsx
@@ -0,0 +1,16 @@
+import styles from '@/styles/login.module.css'
+
+export default function Register() {
+    return(
+        <main>
+          <h1 className={styles.Header}>Olá Treinador, Bem vindo ao site Gabrielzinho NEWS :D </h1>
+
+          <br />
+
+          <iframe width="927" height="695" src="https://www.youtube.com/embed/-Z2MYqI3eIY" title="EU QUERO É SEXO UAAAA" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </main>
+    );
+}
+
+
