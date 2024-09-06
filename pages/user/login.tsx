ecf30a5c2d04c1b4678b22244c4d4cc3be7c2405
GabrielHardt321 <142617209+GabrielHardt321@users.noreply.github.com>
2024-09-06 15:37:05 -0300

    commit 06/09/2024

--- pages/user/login.tsx
+++ pages/user/login.tsx
@@ -1,9 +1,34 @@
 import styles from '@/styles/login.module.css'
 
 export default function Login() {
-    return(
+    return (
         <main className={styles.container}>
             <h1 className={styles.title}>Faça Login</h1>
+
+            <br />
+
+            <p>Insira seu e-mail e verificaremos se você já tem uma conta. <br />Caso contrário, criaremos uma nova.</p>
+
+            <br />
+
+
+            <div>
+                <label className={styles.formlabel}>Email</label>
+                <span className={styles.formspan}>Ou faça login com nome de usuário</span>
+                <br />
+                <input type="text" placeholder='Email' className={styles.forminput} />
+            </div>
+
+            <br />
+
+            <div>
+                <label>Senha</label>
+                <input type="text" className={styles.InputPassword} placeholder='Senha' />
+            </div>
+
+            <br />
+
+            <button className={styles.BntRegister}>Entrar</button>
         </main>
     )
 }
